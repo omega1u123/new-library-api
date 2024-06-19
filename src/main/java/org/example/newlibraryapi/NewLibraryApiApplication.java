4f2060a49f314fb90a40e17c78edc8f16ade1613
omega1u123 <gvardian2810@gmail.com>
2024-06-19 23:10:11 +0300

    init

---
+++ src/main/java/org/example/newlibraryapi/NewLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.newlibraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewLibraryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewLibraryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/newlibraryapi/NewLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.newlibraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewLibraryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
