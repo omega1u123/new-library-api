6ac7e52522b2f3434604e3835dec31c16e7c0890
omega1u123 <gvardian2810@gmail.com>
2024-06-23 21:32:17 +0300

    feat(library-api): add BookCommandServiceTests class

---
+++ library-api/src/test/java/org/example/libraryApi/service/BookCommandServiceTests.java
@@ -0,0 +1,39 @@
+package org.example.libraryApi.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.libraryApi.book.domain.repo.BookRepository;
+import org.example.libraryApi.book.service.BookCommandServiceImpl;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+@Slf4j
+public class BookCommandServiceTests {
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @Mock
+    private ModelMapper modelMapper;
+
+    @InjectMocks
+    private BookCommandServiceImpl bookCommandService;
+/*
+    @Test
+    void addBook(){
+        var book = new Book(1, "qwe", "qwe", "asd", "asd", "asd");
+        var bookDTO = new BookDTO(1, "qwe", "qwe", "asd", "asd", "asd");
+
+        Mockito.when(bookRepository.save(book)).thenReturn(true);
+        Mockito.when(modelMapper.map(bookDTO, Book.class))
+                .thenReturn(book);
+
+        assertThat(bookCommandService.addBook(book)).isTrue();
+    }
+*/
+}
