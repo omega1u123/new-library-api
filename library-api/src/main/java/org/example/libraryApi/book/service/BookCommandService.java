5eb26f4efde9575fcdef7b41fae5d283bc2fd0e7
omega1u123 <gvardian2810@gmail.com>
2024-06-23 21:28:19 +0300

    feat(library-api): add BookCommandService interface

---
+++ library-api/src/main/java/org/example/libraryApi/book/service/BookCommandService.java
@@ -0,0 +1,11 @@
+package org.example.libraryApi.book.service;
+
+import org.example.libraryApi.book.domain.Book;
+
+public interface BookCommandService {
+    Book addBook(Book book);
+    void takeBook(int id);
+    void returnBook(Book book);
+    boolean deleteBook(int id);
+    Book update(int id, Book book);
+}
