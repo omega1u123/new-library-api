dc1060a45fa64648a97b8110fd40f1fa1d1e3ba4
omega1u123 <gvardian2810@gmail.com>
2024-06-23 21:06:45 +0300

    feat: updated pom.xml, init.sql, application.properties

--- library-api/src/main/resources/init.sql
+++ library-api/src/main/resources/init.sql
@@ -1,8 +1,9 @@
 create table book(
-    id bigint primary key,
+    id serial primary key,
     isbn varchar not null unique ,
     title varchar not null ,
     genre varchar not null ,
     description varchar not null ,
     author varchar not null
 )
+
