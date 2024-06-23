e952fb4ce24c37e27cede7a0b1f386b4ec71ea53
omega1u123 <gvardian2810@gmail.com>
2024-06-23 21:17:03 +0300

    feat: add files to library-api module

---
+++ library-api/src/main/java/org/example/mainapi/LibraryApiApplication.java
@@ -0,0 +1,14 @@
+package org.example.mainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApiApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApiApplication.class, args);
+    }
+
+}
