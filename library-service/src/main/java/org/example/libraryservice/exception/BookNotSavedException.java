f9ed23daaa7466fd0bdd4070ce2db82848935138
omega1u123 <gvardian2810@gmail.com>
2024-06-25 00:23:38 +0300

    feat(library-service): add BookNotSavedException.java

---
+++ library-service/src/main/java/org/example/libraryservice/exception/BookNotSavedException.java
@@ -0,0 +1,7 @@
+package org.example.libraryservice.exception;
+
+public class BookNotSavedException extends RuntimeException{
+    public BookNotSavedException(){
+        super("book not saved");
+    }
+}
