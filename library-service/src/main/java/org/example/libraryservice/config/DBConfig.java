7cd7d05c5b5a54ca92469c1999f90441cef23cae
omega1u123 <gvardian2810@gmail.com>
2024-06-25 00:19:44 +0300

    feat(library-service): add DBConfig.java

---
+++ library-service/src/main/java/org/example/libraryservice/config/DBConfig.java
@@ -0,0 +1,21 @@
+package org.example.libraryservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJdbcRepositories
+public class DBConfig{
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+
+
+
+}
