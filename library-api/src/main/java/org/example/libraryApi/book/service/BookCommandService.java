3721a57d5ac54f84d1867891a4f04395f3f335e7
omega1u123 <gvardian2810@gmail.com>
2024-06-29 19:15:41 +0300

    feat(library-api): refactor BookCommandService.java.java

--- library-api/src/main/java/org/example/libraryApi/book/service/BookCommandService.java
+++ library-api/src/main/java/org/example/libraryApi/book/service/BookCommandService.java
@@ -4,9 +4,9 @@ import org.example.libraryApi.book.domain.Book;
 
 public interface BookCommandService {
     Book addBook(Book book);
-    void takeBook(int id);
-    void returnBook(Book book);
-    boolean deleteBook(int id);
+    void takeBook(int bookId);
+    void returnBook(int bookId);
+    boolean deleteBook(int bookId);
     Book update(int id, Book book);
 
 }
