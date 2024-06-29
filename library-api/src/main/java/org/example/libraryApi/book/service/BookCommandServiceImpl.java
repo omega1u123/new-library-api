e9e0dc401a476f20d031611d8eb7e295a24414a1
omega1u123 <gvardian2810@gmail.com>
2024-06-29 19:16:10 +0300

    feat(library-api): refactor BookCommandServiceImpl.java.java

--- library-api/src/main/java/org/example/libraryApi/book/service/BookCommandServiceImpl.java
+++ library-api/src/main/java/org/example/libraryApi/book/service/BookCommandServiceImpl.java
@@ -1,44 +1,48 @@
 package org.example.libraryApi.book.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.libraryApi.book.domain.Book;
 import org.example.libraryApi.book.domain.repo.BookRepository;
-import org.modelmapper.ModelMapper;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class BookCommandServiceImpl implements BookCommandService {
 
     private final BookRepository bookRepository;
-    private final ModelMapper modelMapper;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     @Override
     public Book addBook(Book book) {
-        var savedBook = bookRepository.save(book);
-        if(savedBook != null)
-            return book;
-        return null;
+        Book savedBook = bookRepository.save(book);
+        log.info("sending mess tp add_book-topic");
+        kafkaTemplate.send("add_book-topic", String.valueOf(book.getId()));
+        return book;
     }
 
     @Override
-    public void takeBook(int id) {
-
+    public void takeBook(int bookId) {
+        kafkaTemplate.send("take_book-topic", String.valueOf(bookId));
     }
 
     @Override
-    public void returnBook(Book book) {
+    public void returnBook(int bookId) {
+        kafkaTemplate.send("return_book-topic", String.valueOf(bookId));
     }
 
     @Override
-    public boolean deleteBook(int id) {
-        return bookRepository.removeById(id);
+    public boolean deleteBook(int bookId) {
+        kafkaTemplate.send("delete_book-topic", String.valueOf(bookId));
+        return bookRepository.removeById(bookId);
     }
 
     @Override
-    public Book update(int id, Book book) {
-        if(bookRepository.getById(id) != null){
-            var updatedBook = bookRepository.updateBook(id, book);
+    public Book update(int bookId, Book book) {
+        if(bookRepository.getById(bookId) != null){
+            var updatedBook = bookRepository.updateBook(bookId, book);
             if(updatedBook != null)
                 return updatedBook;
             return null;
