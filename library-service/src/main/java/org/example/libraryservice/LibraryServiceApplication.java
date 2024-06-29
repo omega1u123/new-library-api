5f50cc10a50ddc94317fc73937a3550d2eb499d1
omega1u123 <gvardian2810@gmail.com>
2024-06-29 19:20:15 +0300

    feat(library-service): refactor LibraryServiceApplication.java

--- library-service/src/main/java/org/example/libraryservice/LibraryServiceApplication.java
+++ library-service/src/main/java/org/example/libraryservice/LibraryServiceApplication.java
@@ -3,9 +3,11 @@ package org.example.libraryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableJdbcRepositories(basePackages = "org.example.libraryservice.domain.repo")
+@EnableKafka
 public class LibraryServiceApplication {
 
     public static void main(String[] args) {
