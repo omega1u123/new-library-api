b191afc45789fdc4613b92e494de7db34afe293b
omega1u123 <gvardian2810@gmail.com>
2024-07-01 04:16:48 +0300

    feat(library-api): refactor BookQueryServiceImpl.java

--- library-api/src/main/java/org/example/libraryApi/book/service/BookQueryServiceImpl.java
+++ library-api/src/main/java/org/example/libraryApi/book/service/BookQueryServiceImpl.java
@@ -1,12 +1,12 @@
 package org.example.libraryApi.book.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.example.libraryApi.book.domain.Book;
 import org.example.libraryApi.book.domain.repo.BookRepository;
-import org.modelmapper.ModelMapper;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -23,7 +23,6 @@ import java.util.stream.Stream;
 public class BookQueryServiceImpl implements BookQueryService {
 
     private final BookRepository bookRepository;
-    private final ModelMapper modelMapper;
     private final ConcurrentHashMap<String, List<Integer>> response = new ConcurrentHashMap<>();
     private final KafkaTemplate<String, String> kafkaTemplate;
 
@@ -42,14 +41,12 @@ public class BookQueryServiceImpl implements BookQueryService {
 
     @Override
     public Book getBookByIsbn(String isbn) {
-        var book = bookRepository.getByIsbn(isbn);
-        if(book != null)
-            modelMapper.map(book, Book.class);
-        return null;
+        return bookRepository.getByIsbn(isbn);
     }
 
+    @SneakyThrows
     @Override
-    public List<Book> getAvailableBooks() throws InterruptedException {
+    public List<Book> getAvailableBooks(){
 
         String reqId = UUID.randomUUID().toString();
 
@@ -78,13 +75,21 @@ public class BookQueryServiceImpl implements BookQueryService {
 
     @KafkaListener(topics = "available_books_resp-topic", groupId = "4")
     public void listen(ConsumerRecord<String, String> record){
+
         String reqId = new String(record.headers().lastHeader("reqId").value());
+
         try {
+
             List<Integer> books = Stream.of(record.value().split(",")).map(Integer::parseInt).toList();
+
             log.info("message from available_books_resp-topic : {}", books);
+
             response.put(reqId, books);
+
         }catch (NumberFormatException ex){
+
             response.put(reqId, List.of(-1));
+
         }
     }
 }
