d12dccfe0fb5fb38fe9cec8d803fbe43572bd11f
omega1u123 <gvardian2810@gmail.com>
2024-07-01 04:17:33 +0300

    feat(library-api): add GraphqlExceptionHandler.java

---
+++ library-api/src/main/java/org/example/libraryApi/book/controller/GraphqlExceptionHandler.java
@@ -0,0 +1,58 @@
+package org.example.libraryApi.book.controller;
+
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import graphql.schema.DataFetchingEnvironment;
+import org.example.libraryApi.book.exceptions.*;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.graphql.execution.ErrorType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GraphqlExceptionHandler extends DataFetcherExceptionResolverAdapter {
+
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+
+        if (ex instanceof BookNotSavedException) {
+            return GraphqlErrorBuilder.newError(env)
+                    .message(ex.getMessage())
+                    .errorType(ErrorType.BAD_REQUEST)
+                    .build();
+        }
+        if (ex instanceof BookNotUpdatedException) {
+            return GraphqlErrorBuilder.newError(env)
+                    .message(ex.getMessage())
+                    .errorType(ErrorType.BAD_REQUEST)
+                    .build();
+        }
+        if (ex instanceof BookNotTookException) {
+            return GraphqlErrorBuilder.newError(env)
+                    .message(ex.getMessage())
+                    .errorType(ErrorType.BAD_REQUEST)
+                    .build();
+        }
+        if (ex instanceof BookNotReturnedException) {
+            return GraphqlErrorBuilder.newError(env)
+                    .message(ex.getMessage())
+                    .errorType(ErrorType.BAD_REQUEST)
+                    .build();
+        }
+        if (ex instanceof BookNotDeletedException) {
+            return GraphqlErrorBuilder.newError(env)
+                    .message(ex.getMessage())
+                    .errorType(ErrorType.BAD_REQUEST)
+                    .build();
+        }
+
+        if (ex instanceof InterruptedException) {
+            return GraphqlErrorBuilder.newError(env)
+                    .message(ex.getMessage())
+                    .errorType(ErrorType.BAD_REQUEST)
+                    .build();
+        }
+
+        return null;
+    }
+
+}
