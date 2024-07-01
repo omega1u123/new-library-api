c2af8990e2fd8c367a6c5c9d0f38df0d03ac9182
omega1u123 <gvardian2810@gmail.com>
2024-07-01 04:16:10 +0300

    feat(library-api): refactor BookCommandServiceImpl.java

--- library-api/src/main/java/org/example/libraryApi/book/service/BookCommandServiceImpl.java
+++ library-api/src/main/java/org/example/libraryApi/book/service/BookCommandServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.libraryApi.book.domain.Book;
 import org.example.libraryApi.book.domain.repo.BookRepository;
+import org.example.libraryApi.book.exceptions.*;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -17,36 +18,48 @@ public class BookCommandServiceImpl implements BookCommandService {
 
     @Override
     public Book addBook(Book book) {
-        Book savedBook = bookRepository.save(book);
-        log.info("sending mess tp add_book-topic");
-        kafkaTemplate.send("add_book-topic", String.valueOf(book.getId()));
-        return book;
+        try {
+            bookRepository.save(book);
+            log.info("sending mess tp add_book-topic");
+            kafkaTemplate.send("add_book-topic", String.valueOf(book.getId()));
+            return book;
+        }catch (RuntimeException ex){
+            throw new BookNotSavedException();
+        }
     }
 
     @Override
     public void takeBook(int bookId) {
-        kafkaTemplate.send("take_book-topic", String.valueOf(bookId));
+        try {
+            kafkaTemplate.send("take_book-topic", String.valueOf(bookId));
+        }catch (RuntimeException ex){
+            throw new BookNotTookException();
+        }
     }
 
     @Override
     public void returnBook(int bookId) {
-        kafkaTemplate.send("return_book-topic", String.valueOf(bookId));
+        try {
+            kafkaTemplate.send("return_book-topic", String.valueOf(bookId));
+        }catch (RuntimeException ex){
+            throw new BookNotReturnedException();
+        }
     }
 
     @Override
     public boolean deleteBook(int bookId) {
         kafkaTemplate.send("delete_book-topic", String.valueOf(bookId));
-        return bookRepository.removeById(bookId);
+        if(!bookRepository.removeById(bookId))
+            throw new BookNotDeletedException();
+        return true;
     }
 
     @Override
     public Book update(int bookId, Book book) {
         if(bookRepository.getById(bookId) != null){
-            var updatedBook = bookRepository.updateBook(bookId, book);
-            if(updatedBook != null)
-                return updatedBook;
-            return null;
+            return bookRepository.updateBook(bookId, book);
+        }else{
+            throw new BookNotUpdatedException();
         }
-        return null;
     }
 }
