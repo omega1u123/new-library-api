b0680d4076dd11c3354221bb8c098ca1f6cf660c
omega1u123 <gvardian2810@gmail.com>
2024-07-01 20:52:08 +0300

    feat(library-api): UserRepo.java, UserDetailsImpl.java, UserDetailsServiceImpl.java

---
+++ library-api/src/main/java/org/example/libraryApi/user/service/UserDetailsImpl.java
@@ -0,0 +1,52 @@
+package org.example.libraryApi.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.libraryApi.user.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
