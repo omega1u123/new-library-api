8d4cd5b05e67a61bc84359bacefa1705150de3f6
omega1u123 <gvardian2810@gmail.com>
2024-07-06 02:12:47 +0300

    feat(library-api): refactor GraphqlExceptionHandler.java

--- library-api/src/main/java/org/example/libraryApi/book/controller/GraphqlExceptionHandler.java
+++ library-api/src/main/java/org/example/libraryApi/book/controller/GraphqlExceptionHandler.java
@@ -14,44 +14,13 @@ public class GraphqlExceptionHandler extends DataFetcherExceptionResolverAdapter
     @Override
     protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
 
-        if (ex instanceof BookNotFoundException) {
-            return GraphqlErrorBuilder.newError(env)
-                    .message(ex.getMessage())
-                    .errorType(ErrorType.BAD_REQUEST)
-                    .build();
-        }
-        if (ex instanceof BookNotSavedException) {
-            return GraphqlErrorBuilder.newError(env)
-                    .message(ex.getMessage())
-                    .errorType(ErrorType.BAD_REQUEST)
-                    .build();
-        }
-        if (ex instanceof BookNotUpdatedException) {
-            return GraphqlErrorBuilder.newError(env)
-                    .message(ex.getMessage())
-                    .errorType(ErrorType.BAD_REQUEST)
-                    .build();
-        }
-        if (ex instanceof BookNotTookException) {
-            return GraphqlErrorBuilder.newError(env)
-                    .message(ex.getMessage())
-                    .errorType(ErrorType.BAD_REQUEST)
-                    .build();
-        }
-        if (ex instanceof BookNotReturnedException) {
-            return GraphqlErrorBuilder.newError(env)
-                    .message(ex.getMessage())
-                    .errorType(ErrorType.BAD_REQUEST)
-                    .build();
-        }
-        if (ex instanceof BookNotDeletedException) {
-            return GraphqlErrorBuilder.newError(env)
-                    .message(ex.getMessage())
-                    .errorType(ErrorType.BAD_REQUEST)
-                    .build();
-        }
-
-        if (ex instanceof InterruptedException) {
+        if (ex instanceof BookNotFoundException
+                || ex instanceof BookNotSavedException
+                || ex instanceof BookNotUpdatedException
+                || ex instanceof BookNotTookException
+                || ex instanceof BookNotReturnedException
+                || ex instanceof BookNotDeletedException
+                || ex instanceof InterruptedException) {
             return GraphqlErrorBuilder.newError(env)
                     .message(ex.getMessage())
                     .errorType(ErrorType.BAD_REQUEST)
