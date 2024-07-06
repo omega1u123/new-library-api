5cc787713b47a3733ac21d10838f5854597e8f5d
omega1u123 <gvardian2810@gmail.com>
2024-07-06 19:25:06 +0300

    feat(library-service): add BookController.java

---
+++ library-service/src/main/java/org/example/libraryservice/controller/BookController.java
@@ -0,0 +1,26 @@
+package org.example.libraryservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.libraryservice.exception.BookNotFoundException;
+import org.example.libraryservice.service.BookQueryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookQueryService bookQueryService;
+
+    @GetMapping("/getFreeBooks")
+    public ResponseEntity<?> getFreeBooks() {
+        try {
+            return ResponseEntity.ok(bookQueryService.getFreeBooks());
+        } catch (BookNotFoundException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        }
+    }
+
+}
