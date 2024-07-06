12a5570b3c0a8ff52ea9ef3b8521221bed250ba5
omega1u123 <gvardian2810@gmail.com>
2024-07-07 01:48:35 +0300

    feat(library-api): refactor LibraryApiApplication.java

--- library-api/src/main/java/org/example/libraryApi/LibraryApiApplication.java
+++ library-api/src/main/java/org/example/libraryApi/LibraryApiApplication.java
@@ -2,8 +2,10 @@ package org.example.libraryApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class LibraryApiApplication {
 
     public static void main(String[] args) {
