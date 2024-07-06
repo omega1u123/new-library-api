7f85aa0182627fa08a84e4c7cd927d222431724f
omega1u123 <gvardian2810@gmail.com>
2024-07-07 01:45:41 +0300

    feat(library-api): formatted code in BookCommandService.java

--- library-api/src/main/java/org/example/libraryApi/book/service/BookCommandService.java
+++ library-api/src/main/java/org/example/libraryApi/book/service/BookCommandService.java
@@ -4,9 +4,12 @@ import org.example.libraryApi.book.domain.Book;
 
 public interface BookCommandService {
     Book addBook(Book book);
-    void takeBook(int bookId);
-    void returnBook(int bookId);
-    boolean deleteBook(int bookId);
-    Book update(int id, Book book);
 
+    void takeBook(int bookId);
+
+    void returnBook(int bookId);
+
+    boolean deleteBook(int bookId);
+
+    Book update(int id, Book book);
 }
