f8d4c337094b3890b8622310786c5bfb9d64e196
omega1u123 <gvardian2810@gmail.com>
2024-07-07 01:52:00 +0300

    feat(library-service): refactor BookQueryServiceImpl.java

--- library-service/src/main/java/org/example/libraryservice/service/BookQueryServiceImpl.java
+++ library-service/src/main/java/org/example/libraryservice/service/BookQueryServiceImpl.java
@@ -2,6 +2,7 @@ package org.example.libraryservice.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.libraryservice.domain.repo.BookRepository;
+import org.example.libraryservice.exception.BookNotFoundException;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +10,16 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class BookQueryServiceImpl implements BookQueryService{
+public class BookQueryServiceImpl implements BookQueryService {
 
     private final BookRepository bookRepository;
 
     @Override
     public List<Integer> getFreeBooks() {
-        return bookRepository.getFreeBooks();
+        List<Integer> freeBooks = bookRepository.getFreeBooks();
+        if (freeBooks == null) {
+            throw new BookNotFoundException();
+        }
+        return freeBooks;
     }
 }
