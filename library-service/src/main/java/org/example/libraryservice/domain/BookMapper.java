76d777131fe00e8a5b77915c62492045b437344c
omega1u123 <gvardian2810@gmail.com>
2024-07-06 19:25:50 +0300

    feat(library-service): refactor BookMapper.java

--- library-service/src/main/java/org/example/libraryservice/domain/BookMapper.java
+++ library-service/src/main/java/org/example/libraryservice/domain/BookMapper.java
@@ -1,17 +1,30 @@
 package org.example.libraryservice.domain;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Slf4j
 public class BookMapper implements RowMapper<Book> {
 
     @Override
     public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return new Book(rs.getInt("id"),
-                rs.getDate("taken_date").toLocalDate(),
-                rs.getDate("return_date").toLocalDate()
-        );
+        var book = new Book();
+        book.setId(rs.getInt("id"));
+        log.info("BookMapper mapRow() call");
+        try {
+            log.info("getDate != null, {}", rs.getDate("taken_date").toLocalDate());
+            book.setTakenDate((rs.getDate("taken_date").toLocalDate()));
+            book.setReturnDate(rs.getDate("return_date").toLocalDate());
+            return book;
+        } catch (NullPointerException ex) {
+            log.info("BookMapper nullPointer ex");
+            book.setTakenDate(null);
+            book.setReturnDate(null);
+            return book;
+        }
+
     }
 }
