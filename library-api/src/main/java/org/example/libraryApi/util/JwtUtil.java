c8e6fb67f7c007768b5f6ad2fb47fc9d478d9642
omega1u123 <gvardian2810@gmail.com>
2024-07-07 01:26:23 +0300

    feat(library-api): refactor JwtUtil.java

--- library-api/src/main/java/org/example/libraryApi/util/JwtUtil.java
+++ library-api/src/main/java/org/example/libraryApi/util/JwtUtil.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.NonNull;
+import org.example.libraryApi.security.UserRoles;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -21,30 +22,36 @@ public class JwtUtil {
 
     private final SecretKey jwtAccessSecret;
     private final SecretKey jwtRefreshSecret;
+    private final int accessTokenExpirationDays;
+    private final int refreshTokenExpirationDays;
 
     public JwtUtil(
             @Value("${jwt.secret.access}") String jwtAccessSecret,
-            @Value("${jwt.secret.refresh}") String jwtRefreshSecret) {
+            @Value("${jwt.secret.refresh}") String jwtRefreshSecret,
+            @Value("${accessToken.expiration.days}") int accessTokenExpirationDays,
+            @Value("${refreshToken.expiration.days}") int refreshTokenExpirationDays) {
         this.jwtAccessSecret = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtAccessSecret));
         this.jwtRefreshSecret = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtRefreshSecret));
+        this.accessTokenExpirationDays = accessTokenExpirationDays;
+        this.refreshTokenExpirationDays = refreshTokenExpirationDays;
     }
 
-    public String generateAccessToken(@NonNull UserDetails user){
+    public String generateAccessToken(@NonNull UserDetails user) {
         final LocalDateTime now = LocalDateTime.now();
-        final Instant accessExpirationInstant = now.plusDays(1).atZone(ZoneId.systemDefault()).toInstant();
+        final Instant accessExpirationInstant = now.plusDays(accessTokenExpirationDays).atZone(ZoneId.systemDefault()).toInstant();
         final Date accessExpiration = Date.from(accessExpirationInstant);
         return Jwts.builder()
                 .subject(user.getUsername())
                 .expiration(accessExpiration)
                 .signWith(jwtAccessSecret)
-                .claim("roles", "USER")
+                .claim("roles", UserRoles.USER)
                 .compact();
     }
 
 
-    public String generateRefreshToken(@NonNull UserDetails user){
+    public String generateRefreshToken(@NonNull UserDetails user) {
         final LocalDateTime now = LocalDateTime.now();
-        final Instant refreshExpirationInstant = now.plusDays(10).atZone(ZoneId.systemDefault()).toInstant();
+        final Instant refreshExpirationInstant = now.plusDays(refreshTokenExpirationDays).atZone(ZoneId.systemDefault()).toInstant();
         final Date refreshExpiration = Date.from(refreshExpirationInstant);
         return Jwts.builder()
                 .subject(user.getUsername())
@@ -53,22 +60,22 @@ public class JwtUtil {
                 .compact();
     }
 
-    public boolean validateAccessToken(@NonNull String accessToken){
+    public boolean validateAccessToken(@NonNull String accessToken) {
         return validateToken(accessToken, jwtAccessSecret);
     }
 
-    public boolean validateRefreshToken(@NonNull String refreshToken){
+    public boolean validateRefreshToken(@NonNull String refreshToken) {
         return validateToken(refreshToken, jwtRefreshSecret);
     }
 
-    private boolean validateToken(@NonNull String token, @NonNull Key secret){
-        try{
+    private boolean validateToken(@NonNull String token, @NonNull Key secret) {
+        try {
             Jwts.parser()
                     .setSigningKey(secret)
                     .build()
                     .parseClaimsJws(token);
             return true;
-        }catch (RuntimeException ex) {
+        } catch (RuntimeException ex) {
             return false;
         }
     }
